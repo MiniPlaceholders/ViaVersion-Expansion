4db0d526952d9d02e9881bb1235d04a1b18cd14d
Adrian Gonzales <adriangonzalesval@gmail.com>
2025-08-20 14:03:40 -0500

    fix: Fixed ExpansionProvider loading

--- src/main/java/io/github/miniplaceholders/expansion/viaversion/ViaVersionExpansionProvider.java
+++ src/main/java/io/github/miniplaceholders/expansion/viaversion/ViaVersionExpansionProvider.java
@@ -13,25 +13,33 @@ public final class ViaVersionExpansionProvider implements ExpansionProvider {
 
     @Override
     public Expansion provideExpansion() {
-        final ViaAPI<?> api = Via.getAPI();
-        return Expansion.builder("viaversion")
-                .audiencePlaceholder("player_protocol_version", (aud, queue, ctx) -> {
-                    final ProtocolVersion version = aud.get(Identity.UUID)
-                            .map(api::getPlayerVersion)
-                            .map(ProtocolVersion::getProtocol)
-                            .orElse(ProtocolVersion.unknown);
-                    return Tag.preProcessParsed(version.getName());
-                })
-                .audiencePlaceholder("player_protocol_id", (aud, queue, ctx) -> {
-                    final int id = aud.get(Identity.UUID)
-                            .map(api::getPlayerVersion)
-                            .orElse(-1);
-                    return Tag.preProcessParsed(Integer.toString(id));
-                }).build();
+        return ViaVersionExpansion.viaversion();
     }
 
     @Override
     public LoadRequirement loadRequirement() {
         return LoadRequirement.requiredComplement("viaversion", "ViaVersion");
+    }
+
+    static final class ViaVersionExpansion {
+        private ViaVersionExpansion() {}
+
+        static Expansion viaversion() {
+            final ViaAPI<?> api = Via.getAPI();
+            return Expansion.builder("viaversion")
+                    .audiencePlaceholder("player_protocol_version", (aud, queue, ctx) -> {
+                        final ProtocolVersion version = aud.get(Identity.UUID)
+                                .map(api::getPlayerVersion)
+                                .map(ProtocolVersion::getProtocol)
+                                .orElse(ProtocolVersion.unknown);
+                        return Tag.preProcessParsed(version.getName());
+                    })
+                    .audiencePlaceholder("player_protocol_id", (aud, queue, ctx) -> {
+                        final int id = aud.get(Identity.UUID)
+                                .map(api::getPlayerVersion)
+                                .orElse(-1);
+                        return Tag.preProcessParsed(Integer.toString(id));
+                    }).build();
+        }
     }
 }
